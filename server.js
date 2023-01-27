2879c75fd55d4c07ea7505ef3f3167e9f0948404
ebonygrrl <abooker77@yahoo.com>
2023-01-26 22:05:53 -0600

    installed mysql2, server loaded successfully

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 // app dependencies
 const express = require('express');
+
+// import sequelize connection
+const sequelize = require('./configuration/connection');
+const port = process.env.port || 3001;
+
+
 const app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`);
+});
